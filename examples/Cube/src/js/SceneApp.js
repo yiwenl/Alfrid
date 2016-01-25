03fef91d50ecdb968ecbddb39b2b6d329fedcf82
Wen <Wen@Lins-MacBook-Pro.local>
2016-01-25 17:51:47 +0000

    Add cube example

---
+++ examples/Cube/src/js/SceneApp.js
@@ -0,0 +1,35 @@
+// SceneApp.js
+
+var GL = bongiovi.GL, gl;
+
+function SceneApp() {
+	gl = GL.gl;
+	bongiovi.Scene.call(this);
+
+	window.addEventListener("resize", this.resize.bind(this));
+}
+
+
+var p = SceneApp.prototype = new bongiovi.Scene();
+
+p._initTextures = function() {
+	console.log('Init Textures');
+};
+
+p._initViews = function() {
+	console.log('Init Views');
+	this._vAxis = new bongiovi.ViewAxis();
+	this._vDotPlane = new bongiovi.ViewDotPlane();
+};
+
+p.render = function() {
+	this._vAxis.render();
+	this._vDotPlane.render();
+};
+
+p.resize = function() {
+	GL.setSize(window.innerWidth, window.innerHeight);
+	this.camera.resize(GL.aspectRatio);
+};
+
+module.exports = SceneApp;
