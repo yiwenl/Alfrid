056e09623f33aa82557a4d4765d6ea415f3e7a3b
Wen <Wen@Lins-MacBook-Pro.local>
2016-01-28 12:25:05 +0000

    Add big triangle shader to ShaderLib

--- src/alfrid/tools/ShaderLibs.js
+++ src/alfrid/tools/ShaderLibs.js
@@ -6,6 +6,7 @@ var glslify = require('glslify');
 
 const ShaderLibs = {
 	simpleColorFrag:glslify('../shaders/simpleColor.frag'),
+	bigTriangleVert:glslify('../shaders/bigTriangle.vert'),
 	generalVert:glslify('../shaders/general.vert'),
 	generalNormalVert:glslify('../shaders/generalWithNormal.vert')
 };
