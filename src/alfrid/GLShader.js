5520c00326a7183f2ae0478308084e30d6b2ad73
yiwenl <bongiovi015@gmail.com>
2016-05-25 14:44:46 +0100

    remove array.every

--- src/alfrid/GLShader.js
+++ src/alfrid/GLShader.js
@@ -5,12 +5,17 @@
 import GL from './GLTool';
 const glslify = require('glslify');
 function isSame(array1, array2) {
-	return array1.every((element, index) => {
-		if(element !== array2[index]) {
+	if(array1.length !== array2.length) {
+		return false;
+	}
+
+	for(let i = 0; i < array1.length; i++) {
+		if(array1[i] !== array2[i]) {
 			return false;
 		}
-		return true;
-	});
+	}
+
+	return true;
 };
 
 function addLineNumbers(string) {
