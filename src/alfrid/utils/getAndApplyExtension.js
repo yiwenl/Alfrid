ed511c3e323ab80d332056bfa3b87e7a5d0b3331
wen <bongiovi015@gmail.com>
2017-02-17 23:22:10 +0000

    Clean up codes

--- src/alfrid/utils/getAndApplyExtension.js
+++ src/alfrid/utils/getAndApplyExtension.js
@@ -6,10 +6,7 @@ export default function getAndApplyExtension(gl, name) {
 		return false;
 	}
 	const suffix = name.split('_')[0];
-	const prefix = '_';
 	const suffixRE = new RegExp(`${suffix}$`);
-	const suffixREAttr = new RegExp(`_${suffix}$`);
-	const prefixRE = new RegExp(`^${prefix}`);
 
 	for (const key in ext) {
 		const val = ext[key];
@@ -17,7 +14,7 @@ export default function getAndApplyExtension(gl, name) {
 			const unsuffixedKey = key.replace(suffixRE, '');
 			if (key.substring) {
 				gl[unsuffixedKey] = ext[key].bind(ext);	
-				console.log('Replacing :', key, '=>', unsuffixedKey);
+				// console.log('Replacing :', key, '=>', unsuffixedKey);
 			}
 		}
 	}
