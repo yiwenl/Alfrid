58b7ae9c7aae8ea3ce419b785f54ada5a038b3df
wen <bongiovi015@gmail.com>
2017-04-18 08:08:35 +0100

    Add pass fxaa

--- src/alfrid/post/PassBloom.js
+++ src/alfrid/post/PassBloom.js
@@ -1,14 +1,14 @@
-// PassFxaa.js
+// PassBloom.js
 
 import GL from '../GLTool';
 import Pass from './Pass';
 import fsFxaa from '../shaders/fxaa.frag';
 
-class PassFxaa extends Pass {
+class PassBloom extends Pass {
 	constructor() {
 		super(fsFxaa);
 		this.uniform('uResolution', [1/GL.width, 1/GL.height]);
 	}
 }
 
-export default PassFxaa;
+export default PassBloom;
