cfcb4d86c174d718a6dc7d2c650413f165b9c0be
wen <bongiovi015@gmail.com>
2017-09-22 11:23:41 +0100

    Basic setup

--- dev/main.js
+++ dev/main.js
@@ -1,5 +1,50 @@
 // main.js
-import alfrid, {GL} from '../src/alfrid';
+import './global.scss';
 
-console.log('Alfrid : ', alfrid);
-console.log('GL TOOL :', GL);
+import { GL, BatchDotsPlane, BatchAxis, CameraPerspective, OrbitalControl } from '../src/alfrid';
+import Scheduler from 'scheduling';
+
+
+// create canvas
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+
+// INIT GL
+GL.init(canvas);
+
+
+//	Camera
+const camera = new CameraPerspective();
+camera.setPerspective(Math.PI/4, window.innerWidth/window.innerHeight, .1, 100);
+
+//	Camera control
+const orbControl = new OrbitalControl(camera, window, 15);
+orbControl.rx.value = orbControl.ry.value = .3
+orbControl.radius.value = 5;
+
+
+//	Views
+const batchDots = new BatchDotsPlane();
+const batchAxis = new BatchAxis();
+
+
+//	render loop
+function render() {
+	GL.clear(0, 0, 0, 0);
+	GL.setMatrices(camera);
+	batchDots.draw();
+	batchAxis.draw();
+}
+Scheduler.addEF(render);
+
+
+//	resizing
+window.addEventListener('resize', resize);
+function resize() {
+
+	GL.setSize(window.innerWidth, window.innerHeight);
+	camera.setPerspective(Math.PI/4, GL.aspectRatio, .1, 100);
+
+}
+resize();
