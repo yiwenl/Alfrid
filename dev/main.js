98d4d78c8cd4f02d212f05ddd79c284708e4ff40
wen <bongiovi015@gmail.com>
2017-09-22 10:29:54 +0100

    webpack 3

---
+++ dev/main.js
@@ -0,0 +1,5 @@
+// main.js
+import alfrid, {GL} from '../build/alfrid';
+
+console.log('Alfrid : ', alfrid);
+console.log('GL TOOL :', GL);
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+// webpack.config.js
+const path = require('path');
+const webpack = require('webpack');
+
+const pathOutput = path.resolve(__dirname, 'dev');
+const pathBuild = path.resolve(__dirname, 'build');
+const pathNodeModules = path.resolve(__dirname, 'node_modules');
+
+const env = process.env.NODE_ENV;
+const isProd = env === 'production';
+const libraryName = 'alfrid';
+console.log('Environment isProd :', isProd);
+
+const plugins = isProd ? 
+[	
+	new webpack.optimize.UglifyJsPlugin({
+		sourceMap:false,
+		compress: {
+			drop_debugger: true,
+			drop_console: true,
+			screw_ie8: true
+		},
+		comments:false,
+		mangle:false
+	})
+] : [
+	new webpack.HotModuleReplacementPlugin()
+];
+
+
+const entry = isProd ? {app:`./src/${libraryName}.js`}
+				: {app:'./dev/main.js'};
+const output = isProd ? {
+		path: pathBuild,
+		filename: `./${libraryName}.js`,
+		library: libraryName,
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	} : {
+		filename:'bundle.js',
+		path: pathOutput
+	};
+
+const devtool = 'source-map';
+
+const config = {
+	entry,
+	devtool,
+	devServer: {
+		host:'0.0.0.0',
+		contentBase: './dev',
+		hot:true,
+		disableHostCheck:true
+	},
+	plugins,
+	output,
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				}
+				// exclude: pathNodeModules
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: pathNodeModules
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+				exclude: pathNodeModules
+			},
+			{
+				test: /\.(glsl|vert|frag)$/,
+				use: ["raw-loader", "glslify-loader"],
+				exclude: pathNodeModules
+			}
+		]
+	}
+}
+
+module.exports = config;
