059fe0ccdfa6173d72308d64cb1259eec9f547b1
Yi-Wen Lin <bongiovi015@gmail.com>
2020-02-24 16:54:40 +0000

    add license

--- dev/main.js
+++ dev/main.js
@@ -1,246 +1,41 @@
 // main.js
-import './global.scss';
-import quickSetup from './utils/quickSetup';
-import AssetsLoader from 'assets-loader';
-import alfrid, { GL, Geom, GLShader, TouchDetector, BatchBall, BatchCopy, GLTexture, FboPingPong, Draw } from '../src/alfrid';
-import fs from './test.frag';
-import fsNoise from './noise.frag';
-import fsUV from './uv.frag';
-import fsCopy from './test.frag';
-import vsInstance from './instance.vert';
+import './global.scss'
+import quickSetup from './utils/quickSetup'
+import AssetsLoader from 'assets-loader'
 
-var random = function(min, max) { return min + Math.random() * (max - min);	}
-
-
-let loader, bCopy, texture, textureData, textureHdr, textureVideo, fbo, fboNoise, drawNoise;
-let cube, shader, floor, shaderCopy;
-let hit = vec3.fromValues(999, 999, 99);
-let ball;
-let mtx = mat4.create();
-let x, y;
-mat4.translate(mtx, mtx, vec3.fromValues(1, 0, 0));
-
-let params = {
-	minFilter:'NEAREST',
-	magFilter:'NEAREST',
-	wrapS:'CLAMP_TO_EDGE',
-	wrapT:'CLAMP_TO_EDGE'
-}
-
-function render() {
-	if(!ball) {	return; }
-	if(!texture) { return; }
-	if(!drawNoise) { return; }
-
-
-	// console.log('drawNoise', drawNoise);
-
-	const t = Math.random() > .5 ? texture : fboNoise.getTexture();
-	drawNoise
-	.uniform('uTime', 'float', alfrid.Scheduler.deltaTime)
-	.uniformTexture('texture', t, 0)
-	.draw();
-
-
-	GL.viewport(0, 0, GL.width, GL.height);
-
-	let s = .2;
-	ball.draw([x.value, y.value, 0], [s, s, s], [1, 0, 0]);
-
-
-	s = 1024;
-	
-	if(!bCopy) {
-		bCopy = new alfrid.BatchCopy();
-	}
-	GL.disable(GL.DEPTH_TEST);
-	
-
-	GL.viewport(0, 0, s, s);
-	bCopy.draw(fbo.getTexture());
-	GL.viewport(s, 0, s, s);
-	bCopy.draw(texture);
-	GL.enable(GL.DEPTH_TEST);
-	GL.viewport(0, 0, GL.width, GL.height);
+function render () {
 }
 
 quickSetup(render)
-.then((o)=> {
+  .then((o) => {
+    const assets = [
+      { id: 'image', url: 'assets/img/test.jpg' },
+      { id: 'image1', url: 'assets/img/test1.jpg' },
+      { id: 'image2', url: 'assets/img/test2.jpg' },
+      { id: 'hdr', url: 'assets/img/singleLight.hdr', type: 'binary' }
+    ]
 
-	const assets = [
-		{"id":"image","url":"assets/img/test.jpg"},
-		{"id":"image1","url":"assets/img/test1.jpg"},
-		{"id":"image2","url":"assets/img/test2.jpg"},
-		{"id":"hdr","url":"assets/img/singleLight.hdr","type":"binary"},
-	];
+    new AssetsLoader({
+      assets
+    }).on('error', (err) => {
+      console.log('Error loading :', err)
+    }).on('complete', (o) => {
+      _onAssetsLoaded(o)
+    }).start()
+  })
 
-	loader = new AssetsLoader({
-		assets		
-	}).on('error', (err)=> {
-		console.log('Error loading :', err);
-	}).on('complete', (o) => {
-		_onAssetsLoaded(o);
-	}).start();
-});
-
-
-
-function _onAssetsLoaded(o) {
-	console.table(o);
-	window.assets = o;
-	// console.log('Assets Loaded : ');
-	// console.table(o);
-
-	const img = getAsset('image');
-	// texture = new GLTexture(img);
-
-	// const source = new Float32Array([
-	// 	255, 0, 0, 255,
-	// 	0, 255, 0, 255,
-	// 	0, 0, 255, 255,
-	// 	255, 255, 0, 255
-	// ]);
-
-	// const source1 = [
-	// 	255, 0, 0, 255,
-	// 	0, 255, 0, 255,
-	// 	0, 0, 255, 255,
-	// 	255, 255, 0, 255,
-	// 	0, 255, 255, 255,
-	// 	255, 0, 255, 255
-	// ];
-
-	// const source2 = [
-	// 	255, 0, 0, 255,
-	// 	0, 255, 0, 255,
-	// 	0, 0, 255, 255,
-	// 	255, 255, 0, 255
-	// ];
-
-	// const source3 = [
-	// 	255, 255, 255, 255,
-	// 	0, 0, 0, 255,
-	// 	0, 0, 0, 255,
-	// 	255, 255, 255, 255
-	// ];	
-
-	texture = new GLTexture(img, {minFilter:GL.NEAREST, wrapS:GL.MIRRORED_REPEAT});
-
-	// console.log('texture', texture);
-	// textureData = new GLTexture(source2, {magFilter:GL.NEAREST, minFilter:GL.NEAREST, type:GL.FLOAT});
-
-	// const video = document.createElement("Video");
-	// video.src = 'assets/video/color1.mp4';
-	// video.loop = true;
-	// video.load();
-	// video.play();
-	// textureVideo = new GLTexture(video);
-
-	// const s = 1024;
-	// fbo = new alfrid.FrameBuffer(s, s, {minFilter:GL.LINEAR_MIPMAP_NEAREST});
-
-	const MIP = ['LINEAR', 'NEAREST', 'LINEAR_MIPMAP_LINEAR', 'LINEAR_MIPMAP_NEAREST'];
-	const WRAP = ['CLAMP_TO_EDGE', 'MIRRORED_REPEAT', 'REPEAT'];
-	gui.add(params, 'minFilter', MIP).onChange(o=> {
-		fbo.getTexture().minFilter = GL[o];
-		// fbo.getTexture().showParameters();
-		texture.minFilter = GL[o];
-	});
-
-	gui.add(params, 'magFilter', MIP).onChange(o=> {
-		fbo.getTexture().magFilter = GL[o];
-		// fbo.getTexture().showParameters();
-		texture.magFilter = GL[o];
-	});
-
-	// gui.add(params, 'wrapS', WRAP).onChange(o=> {
-	// 	texture.wrapS = GL[o];
-	// });
-
-	// gui.add(params, 'wrapT', WRAP).onChange(o=> {
-	// 	texture.wrapT = GL[o];
-	// });
-
-
-	ball = new BatchBall();
-	x = new alfrid.SpringNumber(0);
-	y = new alfrid.SpringNumber(0);
-	x.limit(0, 2);
-
-	window.addEventListener('mousedown', (e) => {
-		console.log('mouse down');
-		let r = 2;
-		x.value = random(-r, r);
-		y.value = random(-r, r);
-	})
-
-
-	const size = 256;
-	fbo = new alfrid.FrameBuffer(size, size, {
-		minFilter:GL.NEAREST,
-		magFilter:GL.NEAREST
-	});
-
-	fboNoise = new alfrid.FrameBuffer(size, size, {
-		minFilter:GL.NEAREST,
-		magFilter:GL.NEAREST
-	});
-
-	// const shaderNoise = new alfrid.GLShader(alfrid.ShaderLibs.bigTriangleVert, fsNoise);
-	// const mesh = alfrid.Geom.bigTriangle();
-	// fbo.bind();
-	// shaderNoise.bind();
-	// GL.draw(mesh);
-	// fbo.unbind();
-
-	const positions = [
-		[0, 0, 0],
-		[1, 0, 0],
-		[0, 1, 0]
-	];
-	const uvs = [
-		[0, 0],
-		[1, 0],
-		[0, 1]
-	]
-	const indices = [0, 1, 2];
-
-	const posOffset = [
-		[2, 0, 0],
-		[0, 2, 0],
-		[0, 0, 2]
-	]
-
-	drawNoise = new Draw()
-		.setClearColor(1, 1, 1, .5)
-		// .useProgram(alfrid.ShaderLibs.bigTriangleVert, fsNoise)
-		.useProgram(vsInstance, fsCopy)
-		// .useProgram(shaderNoise)
-		// .setMesh(alfrid.Geom.bigTriangle())
-		.bufferVertex(positions)
-		.bufferTexCoord(uvs)
-		.bufferIndex(indices)
-		.bufferInstance(posOffset, 'aPosOffset')
-		.bindFrameBuffer(fbo)
-		.uniformTexture('texture', texture, 0)
-		.draw();
-
-	new Draw()
-		.useProgram(alfrid.ShaderLibs.bigTriangleVert, fsNoise)
-		.setMesh(alfrid.Geom.bigTriangle())
-		.bindFrameBuffer(fboNoise)
-		.draw();
-
+function _onAssetsLoaded (o) {
+  console.table(o)
+  window.assets = o
 }
 
+function getAsset (id) {
+  const o = assets.find(a => {
+    return a.id === id
+  })
 
-function getAsset(id) {
-	const o = assets.find( a => {
-		return a.id === id;
-	});
-
-	if(!o) {
-		return null;
-	}
-	return o.file;
+  if (!o) {
+    return null
+  }
+  return o.file
 }
--- dev/utils/quickSetup.js
+++ dev/utils/quickSetup.js
@@ -1,78 +1,69 @@
 // quickSetup.js
 
-import { GL, BatchDotsPlane, BatchAxis, CameraPerspective, OrbitalControl } from '../../src/alfrid';
-import Scheduler from 'scheduling';
-import dat from 'dat-gui';
+import { GL, BatchDotsPlane, BatchAxis, CameraPerspective, OrbitalControl } from '../../src/alfrid'
+import Scheduler from 'scheduling'
+import dat from 'dat-gui'
 /*
 export default (mRender, mCallback, mResize) => {
-	
+
 }
 
 */
 
-
 const quickSetup = (mRender, mResize) => new Promise((resolve, reject) => {
-	// create canvas
-	const canvas = document.createElement("canvas");
-	document.body.appendChild(canvas);
+  // create canvas
+  const canvas = document.createElement('canvas')
+  document.body.appendChild(canvas)
 
+  // INIT GL
+  GL.init(canvas, { useWebgl2: true })
 
-	// INIT GL
-	GL.init(canvas);
+  //	Camera
+  const camera = new CameraPerspective()
+  camera.setPerspective(Math.PI / 4, window.innerWidth / window.innerHeight, 0.1, 100)
 
+  //	Camera control
+  const orbControl = new OrbitalControl(camera, window, 15)
+  orbControl.rx.value = orbControl.ry.value = 0.3
+  orbControl.radius.value = 5
 
-	//	Camera
-	const camera = new CameraPerspective();
-	camera.setPerspective(Math.PI/4, window.innerWidth/window.innerHeight, .1, 100);
+  //	Views
+  const batchDots = new BatchDotsPlane()
+  const batchAxis = new BatchAxis()
 
-	//	Camera control
-	const orbControl = new OrbitalControl(camera, window, 15);
-	orbControl.rx.value = orbControl.ry.value = .3
-	orbControl.radius.value = 5;
+  //	render loop
+  function loop () {
+    GL.clear(0, 0, 0, 0)
+    GL.setMatrices(camera)
+    batchDots.draw()
+    batchAxis.draw()
 
+    if (mRender) {
+      mRender()
+    }
+  }
+  Scheduler.addEF(loop)
 
-	//	Views
-	const batchDots = new BatchDotsPlane();
-	const batchAxis = new BatchAxis();
+  //	resizing
+  window.addEventListener('resize', resize)
+  function resize () {
+    GL.setSize(window.innerWidth, window.innerHeight)
+    camera.setPerspective(Math.PI / 4, GL.aspectRatio, 0.1, 100)
 
+    if (mResize) {
+      mResize()
+    }
+  }
+  resize()
 
-	//	render loop
-	function loop() {
-		GL.clear(0, 0, 0, 0);
-		GL.setMatrices(camera);
-		batchDots.draw();
-		batchAxis.draw();
+  const o = {
+    camera,
+    orbControl
+  }
 
-		if(mRender) {
-			mRender();
-		}
-	}
-	Scheduler.addEF(loop);
+  window.gui = new dat.GUI({ width: 300 })
 
+  resolve(o)
+})
 
-	//	resizing
-	window.addEventListener('resize', resize);
-	function resize() {
-
-		GL.setSize(window.innerWidth, window.innerHeight);
-		camera.setPerspective(Math.PI/4, GL.aspectRatio, .1, 100);
-
-		if(mResize) {
-			mResize()
-		}
-
-	}
-	resize();
-
-	const o = {
-		camera,
-		orbControl
-	}
-
-	window.gui = new dat.GUI({width:300});
-
-	resolve(o);
-});
-
-
-export default quickSetup;
+export default quickSetup
